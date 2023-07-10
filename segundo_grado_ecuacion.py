109a890820a6d6408449843c62f4cb2ce619983c
GeraGHG <gera.GHGhernandez@gmail.com>
2023-07-10 13:01:56 -0600

    corrección de código

--- segundo_grado_ecuacion.py
+++ segundo_grado_ecuacion.py
@@ -1,12 +1,10 @@
-from math import sqrt # La función sqrt calcula la raíz cuadrada de un número.
 print("Programa para la resolución a x*x + b x + c = 0")
-a = float(input("Valor de a: "))
+a = 0
+while a <= 0:
+    a = float(input("Valor de a: "))
 b = float(input("Valor de b: "))
 c = float(input("Valor de c: "))
-if a != 0:
-    x1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
-    x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
-    print("El resultado de x1 es: {0:.3f} y el resultado de x2 es: {0:.3f}".format(x1, x2))
 
-if a == 0:
-    print("No es una ecuación de segundo grado.")
+x1 = (-b + (pow(b, 2) * 4 * a * c) ** 2) / (2 * a)
+x2 =(-b - (pow(b, 2) * 4 * a * c) ** 2) / (2 * a)
+print("El resultado de x1 es: {0:.3f} y el resultado de x2 es: {1:.3f}".format(x1, x2))
